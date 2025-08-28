8b613236a3850d26f9ca65c8c30064f836457402
FireFrog-11 <isaac_w11@outlook.com>
2025-08-28 16:30:30 +1000

    Changes to main.py, UI Component bug fixes
    This commit changes the layout of the main.py file to be more organized. It also fixes a critical bug with the UI Component which made the render and handle event not work for UI Components.

--- components/UI/template/ui_component.py
+++ components/UI/template/ui_component.py
@@ -11,18 +11,17 @@ class UIComponent(ABC):
         self.enabled = enabled
         self.rect = pygame.FRect(0, 0, 0, 0,) # Override in subclasses
 
-    @abstractmethod
     def set_position(self, x: float, y: float):
         self.rect.topleft = (x, y)
 
-    @abstractmethod
     def move(self, dx: float, dy: float):
         self.rect.move_ip(dx, dy)
 
     @abstractmethod
     def handle_event(self, event: pygame.event.Event):
         if not self.visible or not self.enabled:
-            return
+            return False
+        return True
     
     @abstractmethod
     def update(self, dt: float):
@@ -31,4 +30,5 @@ class UIComponent(ABC):
     @abstractmethod
     def render(self, surface: pygame.Surface):
         if not self.visible:
-            return
+            return False
+        return True
--- main.py
+++ main.py
@@ -2,20 +2,18 @@ import pygame
 import json
 import sys
 
-with open("config/settings.json", "r") as f:
-    file = json.load(f)
-    settings = file["general_settings"]
-
 class Main:
     def __init__(self):
         pygame.init()
+
+        self.settings = self.load_settings()
 
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.clock = pygame.time.Clock()
 
     def run(self):
         while  True:
-            self.clock.tick(settings["fps"])
+            self.clock.tick(self.settings["fps"])
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -26,6 +24,20 @@ class Main:
                     if event.key == pygame.K_ESCAPE:
                         pygame.quit()
                         sys.exit()
+
+            pygame.display.update()
+
+    def load_settings(self):
+        """
+        This is a temp function to load settings json.
+
+        It will be replaced once the save/load system has been made.
+        """
+        with open("config/settings.json", "r") as f:
+            file = json.load(f)
+            settings = file["general_settings"]
+        
+        return settings
 
 if __name__ == "__main__":
     main = Main()
